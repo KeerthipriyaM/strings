53ebb360cbcccb26fb38ec99da1f8139514f12b2
KeerthipriyaM <116139300+KeerthipriyaM@users.noreply.github.com>
2022-10-29 23:19:22 -0700

    Add files via upload

---
+++ find the length of the string without using library function.py
@@ -0,0 +1,5 @@
+a=str(input("enter the string:"))
+count=0
+for x in a:
+        count=count+1
+print(count)
---
+++ to count the number of given letters in the word.py
@@ -0,0 +1,7 @@
+a=str(input("enter the word:"))
+b=str(input("enter the letter:"))
+count=0
+for x in a:
+    if(x==b):
+        count=count+1
+print(count)
