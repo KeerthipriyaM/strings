68677c26b7eb9b873aefdb913f7019ffe606793f
KeerthipriyaM <116139300+KeerthipriyaM@users.noreply.github.com>
2022-11-01 22:53:17 -0700

    Add files via upload

---
+++ to count number of letters and digits in the string.py
@@ -0,0 +1,10 @@
+a="keerthi4"
+count=0
+digit=0
+for x in a:
+    if(x=="1" or x=="2" or x=="3" or x=="4" or x=="5" or x=="6" or x=="7" or x=="8" or x=="0"):
+        count=count+1
+    else:
+        digit=digit+1
+print(count)
+print(digit)
